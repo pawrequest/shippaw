ea06ef312a9e809d1fec98a7cbc506d92b168396
Paul-Sees-Solutions <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2022-09-24 07:19:31 +0100

    making lists

--- data/templatDicts.py
+++ data/templatDicts.py
@@ -5,19 +5,22 @@ item_fields = [
     'model',
     'product_type',  # eg radio, megaphone, accessory each has subclass
     'supplier',  # is an organisation
-    'stock',
     'buy_price',
     'buy_price_updated', # date field auto updated
     'sell_prices', # a dictionary
 
 ]
 
+product_types = ['Radio','Megaphone','charger','battery','audioAcc']
+
 # radio is an item product_type
 radio_fields = [
     'accPort',
     'battery', # class
     'band',
-    'charger'  # class
+    'charger' # is a class
+    'variant' # fuck you hytera
+    'cps' # programming software
 ]
 
 # charger is an item product_type
@@ -35,6 +38,8 @@ battery_fields =[
     'charged_by'
 ]
 
+# audioAcc is an item
+audioAcc_fields = ['suits', 'format', 'accPort']
 
 ## prices
 price_fields = [
@@ -56,8 +61,30 @@ PD705 = {
     'band': 'uhf'
 }
 
-person_fields = ['name', 'phone']
-# phone = dict eg {'mobile' : 077664646587, 'office':5357435}
+## people
+person_fields = [
+    'name',
+    'phone', # dict of 'tel_type' : 07864654465 etc
+    'email'
+
+]
+
+## organisations
+org_fields = []
+
+# inventoryItem is an individual physical object
+invenItem_fields = [
+    'serial',
+    'owner',
+    'our_purchase_date'
+    'our_sale_date'
+]
 
 
-RADIOBINS = {'A66' : "somepath"}
+# codeplug
+codeplug_fields = [
+    'customer',
+    'radio',
+    'channels' # dict of channel info
+
+]
