430ff547816f67b308db44cffd9e9eba957c6504
Paul-Sees-Solutions <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2022-10-15 14:15:38 +0100

    repairs#

--- AmDespMain.py
+++ AmDespMain.py
@@ -1,16 +1,15 @@
 from python.AmDespProgrammer import ProgrammingAssistant
-from python.AmDespShipper import ShippingApp
 
 
 def shipper(ship_mode):
     # return 1001
-    app = ShippingApp(ship_mode)
-
-    app.xml_to_shipment_()
-    if app.queue_shipment_():
-        if app.book_collection_():
-            print("success")
-    app.log_json()
+    # app = ShippingApp(ship_mode)
+    #
+    # app.xml_to_shipment_()
+    # if app.queue_shipment_():
+    #     if app.book_collection_():
+    #         print("success")
+    # app.log_json()
     return "this is annoying"
 
 
