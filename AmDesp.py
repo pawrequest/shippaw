0a42804e2c213b244ccc62f0591a8e2e7762d428
Paul <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2023-03-13 19:09:23 +0000

    about to play paths

--- AmDesp.py
+++ AmDesp.py
@@ -1,20 +1,21 @@
 import sys
 import os
 from py_code.AmDespShipper import ShippingApp
+
+
 def shipper(ship_mode, xmlfile):
     app = ShippingApp(ship_mode)
     app.run(xmlfile)
 
 
-def test():
-    print("SOMETHNG")
-
 if __name__ == '__main__':
-    if len(sys.argv) >1:
+    if len(sys.argv) > 1:
         shipmode = sys.argv[1]
         xmlfile = sys.argv[2]
     else:
         shipmode = 'sand'
         xmlfile = 'C:\AmDesp\data\AmShip.xml'
     shipper(ship_mode=shipmode, xmlfile=xmlfile)
-    # test()
+
+# todo [mobility] - repo on ryzen is e/dev/, at amherst is c/paul ... use c/pss on both / all systems? or home?
+# todo package imports / sys.path etc. amdesp.spec[pathex].now = e/dev
