a582ae13eaa6e3928c73ad6ab7ee0e4b497bd79d
Paul-Sees-Solutions <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2023-03-30 01:23:53 +0100

    removed .env!

--- amdesp/gui_layouts.py
+++ amdesp/gui_layouts.py
@@ -1,120 +1,6 @@
 import PySimpleGUI as sg
 
 
-def shipment_ids_frame():
-    layout = []
-    for direction in ['inbound', 'outbound']:
-        layout.append(
-            [sg.T('', font='Rockwell 20', size=20, border_width=3, justification='center', relief=sg.RELIEF_GROOVE,
-                  pad=20, k=f'-{direction.upper()}_ID-')])
-
-    frame = sg.Frame('Shipment IDs', layout, pad=20, element_justification='center',
-                     border_width=8,
-                     relief=sg.RELIEF_GROOVE, expand_y=True)
-    return frame
-
-
-def tracking_viewer_window(shipment_id, client):
-    try:
-        shipment = client.get_shipment(shipment_id)
-        tracking_numbers = [parcel.tracking_number for parcel in shipment.parcels]
-        tracking_d = {}
-        sublayout = []
-        parcel_frame = None
-        signatory = None
-        params = {}
-        for tracked_parcel in tracking_numbers:
-            tracking = client.get_tracking(tracked_parcel)
-            # courier = tracking['CourierName'] # debug unused?
-            # parcel_title = [f'{tracked_parcel} ({courier}):'] # debug unused?
-            history = tracking['TrackingHistory']
-            for event in history:
-                if 'delivered' in event.Description.lower():
-                    signatory = f"{chr(10)}Signed for by: {event.Signatory}"
-                    params.update({'background_color': 'aquamarine', 'text_color': 'red'})
-
-                event_text = sg.T(
-                    f'{event.Date} - {event.Description} in {event.Location}{signatory if signatory else ""}',
-                    **params)
-                sublayout.append([event_text])
-
-            parcel_frame = [sg.Column(sublayout)]
-            tracking_d.update({tracked_parcel: tracking})
-
-    except Exception as e:
-        sg.popup_error(e)
-    else:
-        shipment.tracking_dict = tracking_d
-        tracking_window = sg.Window('', [parcel_frame])
-        tracking_window.read()
-
-
-def shipment_name_element():
-    element = sg.Text('', expand_x=True, expand_y=True, justification='center',
-                      font='Rockwell 30',
-                      border_width=8, relief=sg.RELIEF_GROOVE, k='-SHIPMENT_NAME-')
-    return element
-
-
-def combo_popup(options_dict: dict) -> object or None:
-    """
-    Creates a popup window with a sg.Combo() selector and waits for the user to select an option.
-    Returns the selected option.
-    :param options_dict: human readable keys and object-friendly values
-    :return: the selected object
-    """
-    selected_option = None
-    default_value = list(options_dict.keys())[0]
-    layout = [
-        [sg.Combo(list(options_dict.keys()), default_value=default_value, enable_events=True, readonly=True,
-                  k='select')],
-    ]
-
-    window = sg.Window('Address Selector', layout)
-
-    while True:
-        event, values = window.read()
-
-        if event == sg.WINDOW_CLOSED:
-            # User closed the window
-            break
-        elif event == 'select':
-            if values:
-                selected_option = values['select']
-            break
-
-    window.close()
-
-    return options_dict.get(selected_option)
-
-
-def date_chooser() -> sg.Combo:
-    """ PSG Combo to select a despatchbay collection date object
-    if match to send_out_date then green, else purple
-    """
-
-    element = sg.Combo(values=[], pad=(10, 5), size=30, readonly=True, k='-DATE-')
-
-    return element
-
-
-def go_button():
-    element = sg.Button(f'Go!', k="-GO-", size=(12, 5), enable_events=True,
-                        expand_y=True, pad=20, )
-    return element
-
-
-def theme_chooser():
-    theme_layout = [[sg.Text("See how elements look under different themes by choosing a different theme here!")],
-                    [sg.Listbox(values=sg.theme_list(),
-                                size=(20, 12),
-                                key='-THEME LISTBOX-',
-                                enable_events=True)],
-                    [sg.Button("Set Theme")]]
-    frame = sg.Frame('Themes', layout=theme_layout)
-    return frame
-
-
 class GuiLayout:
     def __init__(self):
 
@@ -245,3 +131,117 @@ class GuiLayout:
                            k='-QUEUE_OR_BOOK-', **self.option_menu_params)
 
         return element
+
+
+def shipment_ids_frame():
+    layout = []
+    for direction in ['inbound', 'outbound']:
+        layout.append(
+            [sg.T('', font='Rockwell 20', size=20, border_width=3, justification='center', relief=sg.RELIEF_GROOVE,
+                  pad=20, k=f'-{direction.upper()}_ID-')])
+
+    frame = sg.Frame('Shipment IDs', layout, pad=20, element_justification='center',
+                     border_width=8,
+                     relief=sg.RELIEF_GROOVE, expand_y=True)
+    return frame
+
+
+def tracking_viewer_window(shipment_id, client):
+    try:
+        shipment = client.get_shipment(shipment_id)
+        tracking_numbers = [parcel.tracking_number for parcel in shipment.parcels]
+        tracking_d = {}
+        sublayout = []
+        parcel_frame = None
+        signatory = None
+        params = {}
+        for tracked_parcel in tracking_numbers:
+            tracking = client.get_tracking(tracked_parcel)
+            # courier = tracking['CourierName'] # debug unused?
+            # parcel_title = [f'{tracked_parcel} ({courier}):'] # debug unused?
+            history = tracking['TrackingHistory']
+            for event in history:
+                if 'delivered' in event.Description.lower():
+                    signatory = f"{chr(10)}Signed for by: {event.Signatory}"
+                    params.update({'background_color': 'aquamarine', 'text_color': 'red'})
+
+                event_text = sg.T(
+                    f'{event.Date} - {event.Description} in {event.Location}{signatory if signatory else ""}',
+                    **params)
+                sublayout.append([event_text])
+
+            parcel_frame = [sg.Column(sublayout)]
+            tracking_d.update({tracked_parcel: tracking})
+
+    except Exception as e:
+        sg.popup_error(e)
+    else:
+        shipment.tracking_dict = tracking_d
+        tracking_window = sg.Window('', [parcel_frame])
+        tracking_window.read()
+
+
+def shipment_name_element():
+    element = sg.Text('', expand_x=True, expand_y=True, justification='center',
+                      font='Rockwell 30',
+                      border_width=8, relief=sg.RELIEF_GROOVE, k='-SHIPMENT_NAME-')
+    return element
+
+
+def combo_popup(options_dict: dict) -> object or None:
+    """
+    Creates a popup window with a sg.Combo() selector and waits for the user to select an option.
+    Returns the selected option.
+    :param options_dict: human readable keys and object-friendly values
+    :return: the selected object
+    """
+    selected_option = None
+    default_value = list(options_dict.keys())[0]
+    layout = [
+        [sg.Combo(list(options_dict.keys()), default_value=default_value, enable_events=True, readonly=True,
+                  k='select')],
+    ]
+
+    window = sg.Window('Address Selector', layout)
+
+    while True:
+        event, values = window.read()
+
+        if event == sg.WINDOW_CLOSED:
+            # User closed the window
+            break
+        elif event == 'select':
+            if values:
+                selected_option = values['select']
+            break
+
+    window.close()
+
+    return options_dict.get(selected_option)
+
+
+def date_chooser() -> sg.Combo:
+    """ PSG Combo to select a despatchbay collection date object
+    if match to send_out_date then green, else purple
+    """
+
+    element = sg.Combo(values=[], pad=(10, 5), size=30, readonly=True, k='-DATE-')
+
+    return element
+
+
+def go_button():
+    element = sg.Button(f'Go!', k="-GO-", size=(12, 5), enable_events=True,
+                        expand_y=True, pad=20, )
+    return element
+
+
+def theme_chooser():
+    theme_layout = [[sg.Text("See how elements look under different themes by choosing a different theme here!")],
+                    [sg.Listbox(values=sg.theme_list(),
+                                size=(20, 12),
+                                key='-THEME LISTBOX-',
+                                enable_events=True)],
+                    [sg.Button("Set Theme")]]
+    frame = sg.Frame('Themes', layout=theme_layout)
+    return frame
