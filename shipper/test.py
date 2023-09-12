d2e71955433aba062d3939dcd731bbe6ebb4512f
Paul-Sees-Solutions <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2023-09-12 17:49:37 +0100

    relative imprts + tests

---
+++ shipper/test.py
@@ -0,0 +1,26 @@
+import datetime
+from collections import OrderedDict
+
+record_hire = OrderedDict([
+    ('NAME', 'Test - 16/08/2023 ref 31619'), ('DELIVERY_A', '30 bennet close\x00\x00\x00\x00\x00\x00\x00\x00'),
+    ('DELIVERY_C', 'Test\x00\x00\x00\x00\x00\x00\x00\x00'),
+    ('DELIVERY_E', 'dsgdsa@dasgdg.com\x00\x00\x00\x00\x00\x00\x00\x00'),
+    ('DELIVERY_P', 'ME8 8SP\x00\x00\x00\x00\x00\x00\x00\x00'), ('DELIVERY_T', '07666 666666'),
+    ('DELIVERY_N', 'Test\x00\x00\x00\x00\x00\x00\x00\x00'), ('BOXES', '1'), ('STATUS', 'Quote given'),
+    ('FIELD10', 'Test\x00\x00\x00\x00\x00\x00\x00\x00\x00'), ('SEND_OUT_D', datetime.date(2023, 8, 21)),
+    ('INBOUND_ID', None), ('OUTBOUND_I', '100786-6341\r\n100786-6340\r\n100786-6336\x00'), ('CUSTOMER_N', None),
+    ('DB_LABEL_P', True), ('REPROGRAMM', False), ('SENDING_ST', 'Fine no problem')])
+
+record_customer = OrderedDict([
+    ('NAME', 'Showsec International Ltd - 22/08/2023 ref 20139'),
+    ('DELIVERY_A', 'Unit 6, 72 Bradgate Street\r\nLeicester\x00\x00\x00\x00\x00\x00\x00\x00'),
+    ('DELIVERY_C', 'Adam Hepke\x00\x00\x00\x00\x00\x00\x00\x00'),
+    ('DELIVERY_E', 'adam.hepke@showsec.co.uk\x00\x00\x00\x00\x00\x00\x00\x00'),
+    ('DELIVERY_P', 'LE4 0AW\x00\x00\x00\x00\x00\x00\x00\x00'),
+    ('DELIVERY_T', '0116 204 3300'),
+    ('DELIVERY_N', 'Showsec International Ltd\x00\x00\x00\x00\x00\x00\x00\x00'),
+    ('BOXES', '2'), ('STATUS', 'Booked in'), ('FIELD10', 'Showsec International Ltd\x00'),
+    ('SEND_OUT_D', datetime.date(2023, 8, 22)), ('INBOUND_ID', None), ('OUTBOUND_I', None),
+    ('CUSTOMER_N', None), ('DB_LABEL_P', False), ('REPROGRAMM', False),
+    ('SENDING_ST', 'Fine no problem')])
+
