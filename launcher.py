0a5c46f16c0916777a890a141a9517ef81756f2f
prosodyspeaks <prosodyspeaks@gmail.com>
2023-12-03 18:12:03 +0000

    projecttify reclass branch

--- launcher.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-from src.desp_am.main import ShipDirection, ShipMode, ShipmentCategory, main
-
-config, shipments = main(category=ShipmentCategory.HIRE, shipping_mode=ShipMode.SHIP,
-                         direction=ShipDirection.OUT, file=Path('path/to/your/file.dbf'))
-
-...
